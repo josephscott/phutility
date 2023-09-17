3c15cc65343401f0997b417cea27666731b5ced5
Joseph Scott <joseph@josephscott.org>
2023-09-16 20:38:09 -0600

    Just enough to work

--- src/percentiles.php
+++ src/percentiles.php
@@ -2,9 +2,15 @@
 declare( strict_types = 1 );
 
 class Percentiles {
-	private array $data = [];
+	private array $numbers = [];
 
-	public function __construct( array $data ) {
+	public function __construct( array $numbers ) {
+		sort( $numbers );
+		$this->numbers = $numbers;
 	}
 
+	public function get_percentile( int $percentile ) {
+		$index = ( $percentile / 100 ) * ( count( $this->numbers ) - 1 );
+		return $this->numbers[$index];
+	}
 }
