a3763addab6abf085c612615dbe34d9608dc3b07
Joseph Scott <joseph@josephscott.org>
2023-09-26 21:37:06 -0600

    Better test names

--- tests/numberseries-tests.php
+++ tests/numberseries-tests.php
@@ -1,7 +1,7 @@
 <?php
 declare( strict_types = 1 );
 
-test( '1-9, ranked', function() {
+test( '1-9, percentile ranked', function() {
 	$numbers = new NumberSeries( [ 1, 2, 3, 4, 5, 6, 7, 8, 9 ] );
 
 	$result = $numbers->ranked_percentile( .50 );
@@ -25,7 +25,7 @@ test( '1-9, ranked', function() {
 	expect( $result )->toBe( 9 );
 } );
 
-test( '1-9, interpolated', function() {
+test( '1-9, percentile interpolated', function() {
 	$numbers = new NumberSeries( [ 1, 2, 3, 4, 5, 6, 7, 8, 9 ] );
 
 	$result = $numbers->interpolated_percentile( .50 );
@@ -49,7 +49,7 @@ test( '1-9, interpolated', function() {
 	expect( $result )->toBe( 8.92 );
 } );
 
-test( '23 random integers, ranked', function() {
+test( '23 random integers, percentile ranked', function() {
 	$numbers = new NumberSeries( [ 90, 2, 18, 39, 66, 22, 44, 95, 31, 36, 81, 59, 72, 44, 64, 17, 33, 70, 86, 37, 85, 74, 48 ] );
 
 	$result = $numbers->ranked_percentile( .50 );
